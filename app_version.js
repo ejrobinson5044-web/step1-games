bfa67dd9770a27c95a71405ee2f9f0606ce43db1
ejrobinson5044-web <ejrobinson5044@gmail.com>
2026-06-25 14:25:38 -0500

    Bump app version to Push 005

--- app_version.js
+++ app_version.js
@@ -1,6 +1,7 @@
 /* Shared build/push label for Step 1 Arcade. Update this once per deployment. */
 window.STEP1_APP_VERSION = {
-  push: 4,
-  label: "Push 004",
-  slug: "push-004"
+  push: 5,
+  label: "Push 005",
+  slug: "push-005",
+  name: "Command Center"
 };
